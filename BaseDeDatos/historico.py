da0be6430343b869af25a927cc57f3e1b839726e
pedroandresarandamuoz-wq <pedroandres.arandamuoz@gmail.com>
2026-06-15 19:11:51 +0200

    Delete BaseDeDatos directory

--- BaseDeDatos/historico.py
+++
@@ -1,8 +0,0 @@
-# BaseDeDatos/MetaDatos/tiempos.py
-
-def obtener_rango_años():
-    # Genera una lista desde 2008 hasta 2026 inclusive
-    return list(range(2008, 2027))
-
-def obtener_año_actual():
-    return 2026
